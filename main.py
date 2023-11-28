3757e39d00fbd4b9daaaca73a673a7a06271bfc3
MatinChangiz <azizmatteen@gmail.com>
2023-11-28 23:06:48 +0800

    Submit commit

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ varPurchased = []
 vartotal = 0;
 print("Available items in 马丁 shopping system:")
 for key, item in goods.items():
-    print("Items: {key}  ------->  Price:  ${item}")
+    print(f"Items: {key}  ------->  Price:  ${item}")
 
 while True:
     print(f"\nYour balance: ${varBalance}")
-    varInput = input("Enter the item name to buy or (x) to stop shopping: ")
+    varInput = input("Enter the item name to buy or (done) to stop shopping: ")
 
-    if varInput == 'x':
+    if varInput == 'done':
         break
 
     if varInput in goods:
@@ -28,3 +28,5 @@ print("Purchased Goods:")
 print(varPurchased)
 print("Total purchased amount:  ")
 print(vartotal)
+
+# All assignment have been submitted to teacher
